c44b671eb9ea5bfc2d9162d04c12e030601c36b2
bokich <bokov.es@mail.ru>
2019-10-21 17:55:12 +0300

    Last proga v klasse

--- 1.9/Task_6.java
+++ 1.9/Task_6.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
 public class Task_6 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int k=0;
-        int t=0, i ,j, t2=1;
-        int[] a;
-        int N=in.nextInt();
-        a=new int[N];
-        for ( i = 0; i <N; i++) {
-            a[i]=in.nextInt(); }
-        for ( i = 0; i <N; i++) {
-            for (j = 0; j <N; j++) {
-                if(a[i]==a[j]){k++;} }
-            if(t<k){
-                t=k;
-                t2=i;
+        int[][] a;
+        int N = in.nextInt();
+        //int M = in.nextInt();
+        a = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i+j==N-1){a[i][j]=1;}
+                if(i+j>N-1){a[i][j]=2;}
+                if(i+j<N-1){a[i][j]=0;}
             }
-            k=0;
         }
-        System.out.println(a[t2]);
-        System.out.println("Derji, psina");
+            for (int i = 0; i <N; i++) {
+                for (int j =0; j<N; j++) {
+                    System.out.print(a[j][i] + " ");
+                }
+                System.out.println();
+            }
     }
 }
 
